39c75c459cd3a85de9d1dbe728861fcefd302819
Janko <janko.jelic99@gmail.com>
2023-04-22 17:52:07 +0200

    Update lobbies.controller.ts

--- src/lobbies/lobbies.controller.ts
+++ src/lobbies/lobbies.controller.ts
@@ -1,8 +1,19 @@
-import { Body, Controller, Get, Patch, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { LobbiesService } from './lobbies.service';
 import { AdminGuard } from 'src/auth/guards/admin.guard';
 import { Lobby } from './lobby.entity';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('lobbies')
 @Controller('lobbies')
 export class LobbiesController {
   constructor(private lobbiesService: LobbiesService) {}
@@ -18,8 +29,14 @@ export class LobbiesController {
     return await this.lobbiesService.createLobby(lobby);
   }
 
+  @UseGuards(AdminGuard)
   @Patch('/updateLobby')
-  async updateLobby(@Body() body: Partial<Lobby>) {
-    return await this.lobbiesService;
+  async updateLobby(@Param() id: string, @Body() body: Partial<Lobby>) {
+    return await this.lobbiesService.updateLobby(Number(id), body);
+  }
+
+  @Delete('/deleteLobby')
+  async deleteLobby(@Param() id: number) {
+    return await this.lobbiesService.deleteLobby(id);
   }
 }
