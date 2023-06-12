41188382d37ee68bab40e79794db250819097cf3
Janko <janko.jelic99@gmail.com>
2023-06-13 00:39:46 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -35,7 +35,7 @@ async function bootstrap() {
     storageBucket: configService.get('FIREBASE_STORAGE_BUCKET'),
   });
 
-  app.enableCors({ origin: '*' });
+  app.enableCors({ origin: '*', allowedHeaders: '*' });
   await app.listen(configService.get('PORT') || 3000);
 }
 bootstrap();
