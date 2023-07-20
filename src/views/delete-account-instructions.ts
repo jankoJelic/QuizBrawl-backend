b7a7696ea624d4e526295c740e2c03c203228ee0
Janko <janko.jelic99@gmail.com>
2023-07-21 00:08:14 +0200

    Create delete-account-instructions.ts

---
+++ src/views/delete-account-instructions.ts
@@ -0,0 +1,9 @@
+export const DELETE_ACCOUNT_INSTRUCTIONS = `
+<h1>Delete your account - instructions:</h1>
+<p>1. Open side bar in Quiz Clash mobile app by clicking on your avatar</p>
+<p>2. Select "Delete account" button</p>
+<p>3. Enter your password</p>
+<p>4. Click submit</p>
+<br/>
+<p>If you have any difficulties, you can request your account to be deleted at: quizclashgame@gmail.com</p>
+`;
