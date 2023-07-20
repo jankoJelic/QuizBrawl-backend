37de6e21c1b5e0e2e93c78da461d51bc2a8e2baa
Janko <janko.jelic99@gmail.com>
2023-07-21 00:08:19 +0200

    Update pages.controller.ts

--- src/pages/pages.controller.ts
+++ src/pages/pages.controller.ts
@@ -1,16 +1,21 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { Public } from 'src/auth/auth.controller';
+import { DELETE_ACCOUNT_INSTRUCTIONS } from 'src/views/delete-account-instructions';
 import { PRIVACY_POLICY } from 'src/views/privacy-policy';
 
+@Public()
 @Controller('pages')
 export class PagesController {
   constructor() {}
 
-  @Public()
   @Get('/privacy_policy')
-  async renderPrivacyPolicy(@Res() res: Response) {
-    console.log(__dirname);
+  renderPrivacyPolicy(@Res() res: Response) {
     res.send(PRIVACY_POLICY);
+  }
+
+  @Get('/delete_account_instructions')
+  renderDeleteAccountInstructions(@Res() res: Response) {
+    res.send(DELETE_ACCOUNT_INSTRUCTIONS);
   }
 }
